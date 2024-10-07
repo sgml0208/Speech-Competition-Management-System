87bc968afa9b6313517f5894358be9cfaa77f7b8
jlm <2687581360@qq.com>
2024-10-07 15:54:12 +0800

    first commit

---
+++ build/CMakeFiles/speech-system.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/speech-system.dir/main.cpp.obj"
+  "CMakeFiles/speech-system.dir/main.cpp.obj.d"
+  "libspeech-system.dll.a"
+  "speech-system.exe"
+  "speech-system.exe.manifest"
+  "speech-system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/speech-system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
